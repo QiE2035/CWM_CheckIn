fe147d5b1f818fb3379aa5943fca75d3a6229f7f
企鹅2035 <qie2035@qq.com>
2020-11-16 22:57:51 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import httplib,os
 test_data_urlencode = os.environ["cwm_data"]
-print test_data_urlencode
 
-requrl = "http://app.hbooker.com/task/get_sign_record"
-headerdata = {"Content-Type":"application/x-www-form-urlencoded","Accept-Encoding":"gzip","User-Agent":"Android  com.kuangxiangciweimao.novel  2.6.020,vivo, vivo X6D, 22, 5.1","Connection":"Keep-Alive","Content-Length":152}
+requrl = "https://app.hbooker.com/reader/get_task_bonus_with_sign_recommend"
+headerdata = {"Content-Type":"application/x-www-form-urlencoded","User-Agent":"Android  com.kuangxiangciweimao.novel  2.6.020,vivo, vivo X6D, 22, 5.1"}
 
 conn = httplib.HTTPConnection("app.hbooker.com")
 
